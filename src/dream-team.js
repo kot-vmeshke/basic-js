e6f5ae0f85c4608078a4e3ba15212c4848c77ec1
Anna Babay <kot-vmeshke.4@gmail.com>
2022-05-01 20:32:56 +0200

    feat: what season, count cats, dream team soutions

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  let teamName;
+  if (!members) return false;
+  let arr = [];
+  for (let i = 0; i < members.length; i++) {
+      if (typeof members[i] == 'string') {
+        let temp = members[i].trim().toUpperCase();
+        arr.push(temp.slice(0,1));
+    }
+  }
+  arr.sort();
+  teamName = arr.join('');
+  return teamName;
 }
 
 module.exports = {
--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  if(!arguments.length) {
+    return 'Unable to determine the time of year!';
+  } else {
+    if (isNaN(Date.parse(date)) || Object.getOwnPropertyNames(date).length > 0) {
+      throw new Error('Invalid date!');
+    } else {
+      let month = date.getUTCMonth();
+      if (month === 0 || month === 1 || month === 11) {
+        return 'winter';
+      } else if (month === 2 || month === 3 || month === 4) {
+        return 'spring';
+      } else if (month === 5 || month === 6 || month === 7) {
+        return 'summer';
+      } else if (month === 8 || month === 9 || month === 10) {
+        return 'fall';
+      }
+    }
+  }
 }
 
 module.exports = {
